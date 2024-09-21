858e4a70e3efa4d69798ee35732cc343c582bc12
Phan Anh <anhpdhe180659@fpt.edu.vn>
2024-09-21 10:41:05 +0700

    Fix

---
+++ src/Funtiona.java
@@ -0,0 +1,7 @@
+public class Funtiona {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int result = sumOfArray(array);
+        System.out.println("Sum of elements in the array: " + result);
+    } 
+}
